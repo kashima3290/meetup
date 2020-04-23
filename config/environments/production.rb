eb150c0e0aeeac297bce78a62e85eb77dd4ce541
kashimaraiki <raiki3290@gmail.com>
2020-04-23 14:31:15 +0900

    記述指定場所変更

--- config/environments/production.rb
+++ config/environments/production.rb
@@ -91,5 +91,4 @@ Rails.application.configure do
 
   # Do not dump schema after migrations.
   config.active_record.dump_schema_after_migration = false
-  server '18.180.202.49', user: 'ec2-user', roles: %w{app db web}
 end
