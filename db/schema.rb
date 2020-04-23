95d9dce88bc478d71ab6a73198f9690a1ed8cc31
kashimaraiki <raiki3290@gmail.com>
2020-04-23 22:09:27 +0900

    ローカルエラー解消

--- db/schema.rb
+++ db/schema.rb
@@ -12,12 +12,20 @@
 
 ActiveRecord::Schema.define(version: 2020_03_27_082125) do
 
-  create_table "boards", options: "ENGINE=InnoDB DEFAULT CHARSET=utf8", force: :cascade do |t|
-    t.string "text"
+  create_table "board_images", options: "ENGINE=InnoDB DEFAULT CHARSET=utf8", force: :cascade do |t|
+    t.integer "board_id"
+    t.integer "image_id"
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
-    t.integer "user_id"
+  end
+
+  create_table "boards", options: "ENGINE=InnoDB DEFAULT CHARSET=utf8", force: :cascade do |t|
     t.integer "community_id"
+    t.integer "user_id"
+    t.string "text"
+    t.string "image"
+    t.datetime "created_at", null: false
+    t.datetime "updated_at", null: false
     t.string "images"
   end
 
